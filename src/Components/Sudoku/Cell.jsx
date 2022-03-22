32cc1fad8b8af13b5398c241aa440e2d14d4eecc
Zhomart Suleymen <zhomart200275@gmail.com>
2022-03-22 21:46:04 +0600

    second

--- src/Components/Sudoku/Cell.jsx
+++ src/Components/Sudoku/Cell.jsx
@@ -1,13 +1,52 @@
-import React from "react";
+import React, { useEffect, useMemo } from "react";
 
-function Cell({ value, row, col }) {
-    let classes = "cell ";
-    classes += row % 3 == 0 ? "line-top " : "";
-    classes += col % 3 == 0 ? "line-left " : "";
-    classes += row > 0 && row % 8 == 0 ? "line-bottom " : "";
-    classes += col > 0 && col % 8 == 0 ? "line-right " : "";
+function Cell({ value, row, col, selectedCell, selectCell, selectedElement }) {
+    
+    const cellClasses = useMemo(() => {
+        const classes = ["cell"];
+        if (row % 3 == 0) classes.push("line-top");
+        if (col % 3 == 0) classes.push("line-left");
+        if (row > 0 && row % 8 == 0) classes.push("line-bottom");
+        if (col > 0 && col % 8 == 0) classes.push("line-right");
+        return classes;
+    }, []);
 
-    return <div className={classes}>{value}</div>;
+    const style =
+        selectedElement.current != null
+            ? {
+                  backgroundColor:
+                      selectedCell.row == row && selectedCell.col == col
+                          ? "rgb(186,222,251)"
+                          : row == selectedCell.row ||
+                            col == selectedCell.col ||
+                            (row >= selectedCell.blockRowBegin &&
+                                row <= selectedCell.blockRowBegin + 2 &&
+                                col >= selectedCell.blockColBegin &&
+                                col <= selectedCell.blockColBegin + 2)
+                          ? "rgb(226,235,243)"
+                          : value != 0 && value == selectedElement.current.val
+                          ? "rgb(194,215,234)"
+                          : null,
+              }
+            : null;
+
+    return (
+        <div
+            className={cellClasses.join(" ")}
+            onClick={(event) => {
+                selectedElement.current = event.target;
+                selectCell({
+                    row,
+                    col,
+                    blockRowBegin: row - (row % 3),
+                    blockColBegin: col - (col % 3),
+                });
+            }}
+            style={style}
+        >
+            {value != 0 ? value : null}
+        </div>
+    );
 }
 
 export default Cell;
--- src/Components/Sudoku/playground.jsx
+++ src/Components/Sudoku/playground.jsx
@@ -1,30 +1,66 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
+
 import Cell from "./Cell";
 
-const data = [
+let randomData = [
     [0, 0, 0, 0, 0, 0, 0, 9, 0],
-    [0, 0, 1, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0, 2, 0, 0, 0],
+    [0, 4, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 2, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 4, 0, 0, 2, 0, 0, 0],
     [0, 3, 0, 0, 0, 0, 0, 4, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 8, 0],
+    [2, 3, 0, 0, 0, 0, 0, 8, 1],
+];
+
+const initData = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
 ];
 
 function Playground() {
+    const [selectedCell, selectCell] = useState({
+        row: 0,
+        col: 0,
+        blockRowBegin: 0,
+        blockColBegin: 0,
+    });
+
+    const [data, setData] = useState(initData);
+    const selectedElement = useRef(null);
+
     useEffect(() => {
-        // retrive data from backend
+        setData(randomData);
+        document.addEventListener("keypress", (ev) => {
+            if (ev.key > 0 && ev.key <= 9 && selectedElement.current != null) {
+                selectedElement.current.innerText = ev.key;
+            }
+        });
     }, []);
 
     return (
         <div className="playground">
             {data.map((row, rowIdx) => (
                 <div className="row" key={rowIdx}>
-                    {
-                        row.map((val, colIdx) => { return <Cell value={val} key={colIdx} row={rowIdx} col={colIdx}/>; })
-                    }
+                    {row.map((val, colIdx) => (
+                        <Cell
+                            value={val}
+                            key={`${rowIdx}${colIdx}`}
+                            row={rowIdx}
+                            col={colIdx}
+                            selectedCell={selectedCell}
+                            selectCell={selectCell}
+                            selectedElement={selectedElement}
+                        />
+                    ))}
                 </div>
             ))}
         </div>
